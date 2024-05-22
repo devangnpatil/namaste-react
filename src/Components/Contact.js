88d7e5932dc7e00871673d5909890c3272155d77
Devang <devangnpatil@gmail.com>
2024-05-22 20:30:11 +0530

    Add test for Contact component

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,9 +1,23 @@
 import React from "react";
 const Contact = () => {
   return (
-    <div>
-      <h1>Contact</h1>
-      <div>This is contact page</div>
+    <div className="">
+      <h1 className="font-bold text-3xl p-4 m-4">Contact Us Page</h1>
+      <form>
+        <input
+          className="border border-black p-2 m-2"
+          type="text"
+          placeholder="Enter your name"
+        />
+        <input
+          className="border border-black p-2 m-2"
+          type="text"
+          placeholder="Enter your message"
+        />
+        <button className="border border-black p-2 m-2 bg-gray-50 rounded-lg">
+          Submit
+        </button>
+      </form>
     </div>
   );
 };
