f133e444dd074ea9a551377817734d8a399a88be
Devang <devangnpatil@gmail.com>
2024-05-23 09:01:01 +0530

    update contact test

--- src/Components/__tests__/Contact.test.js
+++ src/Components/__tests__/Contact.test.js
@@ -1,20 +1,22 @@
 import { render, screen } from "@testing-library/react";
 import Contact from "../Contact";
 import "@testing-library/jest-dom";
-test("Should load contact us component", () => {
-  render(<Contact />);
-  const heading = screen.getByRole("heading");
-  expect(heading).toBeInTheDocument();
-});
+describe("Contact", () => {
+  it("Should load contact us component", () => {
+    render(<Contact />);
+    const heading = screen.getByRole("heading");
+    expect(heading).toBeInTheDocument();
+  });
 
-test("Should load button inside Contact component", () => {
-  render(<Contact />);
-  const button = screen.getByRole("button");
-  expect(button).toBeInTheDocument();
-});
+  it("Should load button inside Contact component", () => {
+    render(<Contact />);
+    const button = screen.getByRole("button");
+    expect(button).toBeInTheDocument();
+  });
 
-test("Should load 2 input boxes in the contact component", () => {
-  render(<Contact />);
-  const input = screen.getAllByRole("textbox");
-  expect(input).toHaveLength(2);
+  it("Should load 2 input boxes in the contact component", () => {
+    render(<Contact />);
+    const input = screen.getAllByRole("textbox");
+    expect(input).toHaveLength(2);
+  });
 });
